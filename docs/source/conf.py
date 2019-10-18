0213e27dfab4a7b0ebc8f6d22a665b1000a240cb
New Mobility TF 조경진 <kyongjin.jo@sk.com>
2019-10-18 15:21:53 +0900

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'DYRODS'
+project = u'RUUT'
 copyright = u'2019, Kyongjin Jo'
 
 # The version info for the project you're documenting, acts as replacement for
